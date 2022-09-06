114473a50db2acce7ba89215a04a703cf4866b70
gvielza <80008338+gvielza@users.noreply.github.com>
2022-09-06 20:26:47 -0300

    dibujo

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-function escoger(){
-    alert ("dd");
+function dibujarlineas() {
+  const palabra = palabraAleatoria();
+
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(palabra);
+  newDiv.appendChild(newContent); //añade texto al div creado.
+
+  // añade el elemento creado y su contenido al DOM
+  var currentDiv = document.getElementById("palabra");
+  document.body.insertBefore(newDiv, currentDiv);
+  
+}
+
+
+
+
+
+
+//funcion devuelve palabra aleatoria
+function palabraAleatoria() {
+  const ctdadPalabras = 10;
+  const palabras = [
+    "javascript",
+    "html",
+    "css",
+    "programa",
+    "lenguaje",
+    "java",
+    "mouse",
+    "teclado",
+    "desarrollo",
+    "api",
+  ];
+  const indice = Math.round(Math.random() * (ctdadPalabras - 1 - 0) + 0);
+  return palabras[indice];
 }
