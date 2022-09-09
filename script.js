61af50e526345e3cfffc8523f5e1bf5dde6d947e
gvielza <80008338+gvielza@users.noreply.github.com>
2022-09-09 20:37:05 -0300

    2 pantallas

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ function dibujarlineas() {
   newDiv.appendChild(newContent); //añade texto al div creado.
 
   // añade el elemento creado y su contenido al DOM
-  var currentDiv = document.getElementById("palabra");
+  var currentDiv = document.getElementById("uno");
   document.body.insertBefore(newDiv, currentDiv);
+  var uno = document.getElementsByName("uno");
+  uno.setAttribute("visibility","hidden");
   
 }
+function iniciarJuego(){
+  var x = document.getElementById("btn_iniciar");
+   x.style.display = "none";
+   
+   var y = document.getElementById("juego");
+   y.style.visibility="visible"
+  
+}
+setInterval(iniciarJuego(),10000);
 
 
 
